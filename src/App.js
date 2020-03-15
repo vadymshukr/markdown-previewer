03ef9a5bb1c2ffa06a8fdedf5701ab51dfd06fe9
vadymshukr <vadymshukr@gmail.com>
2020-03-15 11:45:55 +0200

    All tests are passed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,66 @@ import './App.css';
 import Editor from '../src/Components/Editor/Editor'
 import Preview from '../src/Components/Preview/Preview'
 const marked = require('marked');
+// ALLOWS LINE BREAKS WITH RETURN BUTTON
+marked.setOptions({
+  breaks: true,
+});
 
+const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      content: ''
+      content: marked(placeholder)
     }
     this.dataUpdate = this.dataUpdate.bind(this);
+    this.dataReset = this.dataReset.bind(this);
   }
 
   dataUpdate(data){
@@ -18,10 +70,16 @@ class App extends React.Component {
       content: marked(data)
     })
   }
+
+  dataReset(){
+    this.setState({
+      content: ''
+    })
+  }
   render(){
     return (
       <div className="App">
-        <Editor dataUpdate={this.dataUpdate}/>
+        <Editor dataUpdate={this.dataUpdate} dataReset={this.dataReset} placeholder={placeholder}/>
         <Preview viewContent={this.state.content}/>
       </div>
     );
--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -5,14 +5,26 @@ class Editor extends React.Component {
     constructor(props){
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
     handleChange(e){
         this.props.dataUpdate(e.target.value);
     }
+
+    handleReset(){
+        this.props.dataReset();
+        
+    }
+
+
     render(){
         return (
             <div>
-                <textarea name="" id="editor" cols="30" rows="10" onChange={this.handleChange}></textarea>
+                <form>
+                    <textarea name="" id="editor" cols="30" rows="10" onChange={this.handleChange} >{this.props.placeholder}</textarea>
+                    <button type="reset" onClick={this.handleReset}>Reset</button>
+                </form>
+                
             </div> 
          );
     }
