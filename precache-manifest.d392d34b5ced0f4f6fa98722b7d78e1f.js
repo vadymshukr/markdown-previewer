b99452a902f197169ee488d66dfaa4371714ba11
vadymshukr <vadymshukr@gmail.com>
2020-03-15 14:07:14 +0200

    Updates

---
+++ precache-manifest.d392d34b5ced0f4f6fa98722b7d78e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f79347b0927174ebadf88d0b2e1174a",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "bf82ba8e0994840e45e9",
+    "url": "/markdown-previewer/static/css/main.74e64832.chunk.css"
+  },
+  {
+    "revision": "0d34ef986207fa768c92",
+    "url": "/markdown-previewer/static/js/2.9194a324.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/markdown-previewer/static/js/2.9194a324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf82ba8e0994840e45e9",
+    "url": "/markdown-previewer/static/js/main.0687b918.chunk.js"
+  },
+  {
+    "revision": "699ce588a95e5b83c15e",
+    "url": "/markdown-previewer/static/js/runtime-main.b388dd98.js"
+  }
+]);
